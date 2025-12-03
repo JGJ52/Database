368b451b5f1668ad77608eebe0b787e508dce028
jgj52 <email@jgj52.hu>
2025-12-03 18:18:22 +0100

    add async things

--- src/main/java/hu/jgj52/database/Database.java
+++ src/main/java/hu/jgj52/database/Database.java
@@ -1,6 +1,9 @@
 package hu.jgj52.database;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.concurrent.CompletableFuture;
 
 public final class Database extends JavaPlugin {
 
@@ -39,5 +42,29 @@ public final class Database extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public CompletableFuture<PostgreSQL.QueryResult> asnycPostgreSQL(PostgreSQL.QueryBuilder query) {
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    try {
+                        return query.execute().get();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .thenApplyAsync(result -> result,r -> Bukkit.getScheduler().runTask(this, r));
+    }
+
+    public CompletableFuture<Redis.QueryResult> asnycRedis(Redis.QueryBuilder query) {
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    try {
+                        return query.execute().get();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .thenApplyAsync(result -> result,r -> Bukkit.getScheduler().runTask(this, r));
     }
 }
